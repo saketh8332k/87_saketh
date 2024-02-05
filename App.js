bbf59e8dee06a83c66cdbd9dade4ce9aff70e3bf
saketh8332k <153748696+saketh8332k@users.noreply.github.com>
2024-02-05 01:50:20 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Week1 from './components/week1';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World!!!!!!</h1><hr></hr>
+     <h4>
+      name: M. Sai Saketh </h4>
+      <h4>R.No: 2211CS010387 </h4>
+      <h4>group: 7A </h4><hr></hr>
+    
+      <Week1/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ week1.js
@@ -0,0 +1,23 @@
+import React from "react" ;
+const MyComponentsWithoutJSX=()=>{
+
+}
+const MyComponentsWithJSX=()=>{
+}
+const MyProfile=()=>{
+}
+function week1(){
+    return(
+    <div ClassName="week1" >
+        <h1>displaying my components without JSx</h1>
+        <MyComponentsWithoutJSX/>
+        <hr/>
+        <h1>displaying my components with JSx</h1>
+        <MyComponentsWithJSX/>
+        <hr/>
+        <h1>displaying my profile</h1>
+        <MyProfile/>
+    </div>
+    );
+}
+export default week1;
